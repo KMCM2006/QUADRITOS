bc3cab8b5308a3ae269e0a9701edcc4254a80c93
3av.arana@gmail.com <3av.arana@gmail.com>
2018-11-02 16:42:23 -0400

    Steps of choose modality to play with opponents in the same computer created

---
+++ features/step_definitions/chooseModalityPlayWithOpponentsSameComputer_steps.rb
@@ -0,0 +1,11 @@
+Given("estoy en la pagina modalidad") do
+    visit('/modality')
+end
+
+When("selecciono la opcion jugar contra Oponentes desde la misma PC") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("se hace visible la opcion para elegir la cantidad de jugadores") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
