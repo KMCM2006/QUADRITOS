99256019c3a03a31f315f7abd0fd7ce281cb0551
3av.arana@gmail.com <3av.arana@gmail.com>
2018-11-03 15:56:28 -0400

    View of choose modality to play with opponents in the different computers created

--- features/step_definitions/chooseModalityPlayWithOpponentsDifferentComputers_steps.rb
+++ features/step_definitions/chooseModalityPlayWithOpponentsDifferentComputers_steps.rb
@@ -1,3 +1,4 @@
 When("selecciono la opcion jugar contra Oponentes desde distinta PC") do
-  pending # Write code here that turns the phrase above into concrete actions
+    page.find("//*[@id='modalitySelector']").click
+    page.find("//*[@id='modalityPlayInDifferentComputer']").click  
 end
