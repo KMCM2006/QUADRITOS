a1ccf806cb4f38c260ffd89accc944a041a05a29
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-04 08:21:40 -0400

    Added class line

---
+++ lib/line.rb
@@ -0,0 +1,26 @@
+class Line
+
+    def initialize(posX1, posX2, posY1, posY2)
+        @posX1 = posX1
+        @posY1 = posY1
+        @posX2 = posX2
+        @posY2 = posY2
+    end
+
+    def getPosX1
+        return @posX1
+    end
+
+    def getPosX2
+        return @posX2
+    end
+
+    def getPosY1
+        return @posY1
+    end
+
+    def getPosY2
+        return @posY2
+    end
+
+end
