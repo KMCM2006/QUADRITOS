1fe1c0b3081f67064498084a9cc9938650d8f37a
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-04 02:31:33 -0400

    Past aceptance test for see names of the players and scores

--- features/step_definitions/startGameBetweenTwoPlayersFromTheSamePc_steps.rb
+++ features/step_definitions/startGameBetweenTwoPlayersFromTheSamePc_steps.rb
@@ -7,11 +7,12 @@ Given("visito la pagina del juego") do
   end
 
   Then("se ve el logo del juego") do
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(page).to have_xpath('/html/body/nav/div[1]')
   end
 
-  Then("se ve el puntaje de {string} inicializado en {int}") do |string, int|
-    pending # Write code here that turns the phrase above into concrete actions
+  Then("se ve el puntaje de {string} inicializado en {int}") do |player, score|
+    expect(page).to have_content(player)
+    expect(page).to have_content(score)
   end
 
   Then("se ve la opcion de {string}") do |string|
