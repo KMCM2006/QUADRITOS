94ffcd480785c168e3150d75a3f49c2b3dbe783d
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-03 23:27:23 -0400

    Past aceptance test for see names of the players

--- features/step_definitions/startGameBetweenTwoPlayersFromTheSamePc_steps.rb
+++ features/step_definitions/startGameBetweenTwoPlayersFromTheSamePc_steps.rb
@@ -1,9 +1,9 @@
 Given("visito la pagina del juego") do
-    pending # Write code here that turns the phrase above into concrete actions
+    visit('/game?numberOfPlayer=2')
   end
 
-  Then("se ve el nombre del {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Then("se ve el nombre del {string}") do |player|
+    expect(page).to have_content(player)
   end
 
   Then("se ve el logo del juego") do
