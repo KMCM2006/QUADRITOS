58aac7328dcb0cbd0dd19783c110f3af54b8c3b5
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-03 23:18:31 -0400

    Adding steps to start game between two players

---
+++ features/step_definitions/startGameBetweenTwoPlayersFromTheSamePc_steps.rb
@@ -0,0 +1,23 @@
+Given("visito la pagina del juego") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then("se ve el nombre del {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then("se ve el logo del juego") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then("se ve el puntaje de {string} inicializado en {int}") do |string, int|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then("se ve la opcion de {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Then("se ve el tablero vacio") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
