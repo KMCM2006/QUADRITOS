b42ded2b8c58a1dc7d2072304989e75eb56fc8c4
MayeGer <maye_gallardo@hotmail.com>
2018-11-06 20:04:48 -0400

    added function verifyHorizontalLineTop

--- lib/board.rb
+++ lib/board.rb
@@ -62,6 +62,32 @@ class Board
         end
     end
 
+    def verifiyHorizontalLineBotton(positions)
+        bottonPositionsH = Array[]
+        rightPositionsV = Array[]
+        leftPositionsV = Array[]
+        bottonPositionsH[0] = positions[0]
+        bottonPositionsH[1] = positions[1]
+        bottonPositionsH[2] = (positions[2].to_i - 50 + 100).to_s 
+        bottonPositionsH[3] = (positions[3].to_i - 50 + 100).to_s
+        rightPositionsV[0] = positions[1]
+        rightPositionsV[1] = positions[1]
+        rightPositionsV[2] = positions[2]
+        rightPositionsV[3] = (positions[3].to_i - 50 + 100).to_s
+        leftPositionsV[0] = positions[0]
+        leftPositionsV[1] = positions[0]
+        leftPositionsV[2] = positions[2]
+        leftPositionsV[3] = (positions[3].to_i - 50 + 100).to_s
+        if exitLineWithPositions(bottonPositionsH)
+            && exitLineWithPositions(leftPositionsV)
+            && exitLineWithPositions(rightPositionsV)
+            return true
+        else
+            return false
+        end
+    end
+
+
     def exitLineWithPositions(positions)
         positions.each do |position|
             if position.to_i < 50
