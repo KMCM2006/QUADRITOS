7e8eb6c94ca08c55b40f4ec74d43049f95c9f8eb
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-07 22:08:54 -0400

    added test to vertical case for make a square

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,4 +24,28 @@ describe 'Pruebas de la clase Board' do
         expect(@board.isLineVisible('H',[300, 350, 100, 100])).to eq true
     end
 
+    it "deberia verificar que la linea con las posiciones '0 100 0 100' sea invalida" do
+        expect(@board.isLineValid([0, 100, 0, 100])).to eq false
+    end
+
+    it "deberia verificar que la linea con las posiciones '300 350 100 100' sea valida'" do
+        expect(@board.isLineValid([300, 350, 100, 100])).to eq true
+    end
+
+    it 'deberio devolver true al armar un cuadrado para el caso vertical derecha' do
+        @board.udpateLine('V',[350, 350, 50, 100])
+        @board.udpateLine('H',[300, 350, 50, 50])
+        @board.udpateLine('V',[300, 300, 50, 100])
+        @board.udpateLine('H',[300, 350, 100, 100])
+        expect(@board.verifyVerticalLineRight([350, 350, 50, 100])).to eq true
+    end
+
+    it 'deberio devolver true al armar un cuadrado para el caso vertical izquierda' do
+        @board.udpateLine('V',[350, 350, 50, 100])
+        @board.udpateLine('H',[300, 350, 50, 50])
+        @board.udpateLine('V',[300, 300, 50, 100])
+        @board.udpateLine('H',[300, 350, 100, 100])
+        expect(@board.verifyVerticalLineLeft([300, 300, 50, 100])).to eq true
+    end
+
 end
