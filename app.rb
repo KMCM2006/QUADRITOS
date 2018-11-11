aea4f92a5087a61082fbfc475b694d8286587341
MayeGer <maye_gallardo@hotmail.com>
2018-11-11 18:39:54 -0400

    testing connection with postresql in heroku¨

--- app.rb
+++ app.rb
@@ -1,12 +1,40 @@
 require './lib/game'
 require 'sinatra'
+require "sinatra/activerecord"
+
+configure :development do
+    set :database, {:adapter => "sqlite3", database: "quadritos.sqlite3"}
+end
+configure :production do
+    set :database, {:adapter => "postgresql", :host => "ec2-50-19-127-158.compute-1.amazonaws.com", :username => "uindpsrrjudfbp", :password => "3a44327cb1b9e41897fef2d9fd560d38727c11045c821cf6186837eb05e73387", :database => "postgres://uindpsrrjudfbp:3a44327cb1b9e41897fef2d9fd560d38727c11045c821cf6186837eb05e73387@ec2-50-19-127-158.compute-1.amazonaws.com:5432/dc0q53sb55drvr"}
+end
+
+class User < ActiveRecord::Base
+end
+
+class Score < ActiveRecord::Base
+end 
 
 class App < Sinatra::Base
+    register Sinatra::ActiveRecordExtension
 
     $game
     @username
     @password
     @confirmPassword
+
+    get '/save' do
+        @score = Score.new
+        @score.avatar = "avatar"
+        @score.name = "baymax"
+        @score.points = "100"
+        @score.save()
+    end
+    
+    get '/list' do
+        @scores = Score.all
+        erb :scores
+    end
 
     get '/' do
         erb :welcome
