cf806101fdbce8375fd23d6efdb022adc90df56c
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-16 08:16:43 -0400

    exec bundle

--- features/step_definitions/restartGame_steps.rb
+++ features/step_definitions/restartGame_steps.rb
@@ -1,5 +1,5 @@
 Given("estoy en la pagina de jugar") do
-    visit('/game?modality=1&numberPlayers=2&name11=Jugador+1&avatar11=avatar1.jpg&name12=Jugador+2&avatar12=avatar1.jpg&name13=Jugador+3&avatar13=avatar1.jpg&name14=Jugador+4&avatar14=avatar1.jpg')
+  visit('/game?modality=1&numberPlayers=2&name11=Jugador+1&avatar11=avatar1.jpg&name12=Jugador+2&avatar12=avatar1.jpg&name13=Jugador+3&avatar13=avatar1.jpg&name14=Jugador+4&avatar14=avatar1.jpg')
 end
 
   When("presiono el boton {string}") do |string|
