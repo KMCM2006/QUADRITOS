8c08231fc5380466ef78772dc62b646f40131874
3av.arana@gmail.com <3av.arana@gmail.com>
2018-11-23 07:53:32 -0400

    Steps of edit user data created

---
+++ features/step_definitions/editUserData_steps.rb
@@ -0,0 +1,55 @@
+Given("visito la pagina editar datos") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver el titulo {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver un campo para ingresar mi nuevo nombre") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver un campo para ingresar mi nueva contrasena") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver un campo para confirmar mi contrasena") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Given("estoy en la pagina editar datos") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("ingreso mi nuevo nombre") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver mi nombre en el campo {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("ingreso mi contrasena") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver mi nuevo nombre {string} en el campo nombre") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver mi nueva contrasena {string} en el campo contrasena") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When("confirmo mi contrasena") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia ver mi nueva contrasena {string} en el campo confirmar contrasena") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Then("deberia enviarme a la pagina de elegir modalidad") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
