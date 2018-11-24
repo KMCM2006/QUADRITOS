63fe4eb85ff17a3b71f0f942297971b6791ea91a
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-24 01:16:09 -0400

    refactoring test to verifySquare just in case horizontal top

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -137,6 +137,13 @@ describe 'Pruebas de la clase Board' do
         expect(@board.verifySquare('H-300_350_100_100')).to eq "HT-true_HB-false_"
     end
 
+    it 'deberia devolver "HT-false_HB-false_" al armar un cuadrado para el caso horizontal superior' do
+        @board.udpateLine('V',[350, 350, 50, 100])
+        @board.udpateLine('V',[300, 300, 50, 100])
+        @board.udpateLine('H',[300, 350, 100, 100])
+        expect(@board.verifySquare('H-300_350_100_100')).to eq "HT-false_HB-false_"
+    end
+
     it 'deberia devolver true al armar un cuadrado para el caso horizontal inferior' do
         @board.udpateLine('V',[350, 350, 50, 100])
         @board.udpateLine('H',[300, 350, 50, 50])
